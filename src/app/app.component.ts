bac1d0c6385c6406ebd3b3b72aaa957dd92da2f6
cynter <cynteran27@gmail.com>
2024-04-23 01:05:13 -0300

    ruteo + quien soy

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,23 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Router, RouterOutlet, RouterModule } from '@angular/router';
+import {MatButton} from '@angular/material/button';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RouterModule, MatButton],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'salaDeJuegos';
+
+  constructor(private router: Router) {
+
+  }
+
+  goTo(path: string) {
+    this.router.navigate([path]);
+  }
 }
---
+++ src/app/componentes/quien-soy/quien-soy.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import {MatButtonModule} from '@angular/material/button';
+import {MatCardModule} from '@angular/material/card';
+
+@Component({
+  selector: 'app-quien-soy',
+  standalone: true,
+  imports: [MatCardModule, MatButtonModule],
+  templateUrl: './quien-soy.component.html',
+  styleUrl: './quien-soy.component.css'
+})
+export class QuienSoyComponent {
+
+}
